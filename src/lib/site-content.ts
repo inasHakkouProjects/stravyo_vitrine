8d9bb04669d3825ec041050b91ab4171ad7d2635
Inas Hakkou <inas.hakkou11@gmail.com>
2026-05-11 20:46:59 +0200

    number changed

--- src/lib/site-content.ts
+++ src/lib/site-content.ts
@@ -226,7 +226,7 @@ export const advantageCards: {
 
 export const footerContact = {
   email: "contact@stravyo.com",
-  phone: "+33 (0)6 25 09 40 91",
+  phone: "+33 (0)6 15 43 48 07",
   address1: "Paris & Île-de-France",
   address2: "Bordeaux & Aquitaine",
   address3: "Lyon & Rhône-Alpes",
